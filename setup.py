5c98adea4e9bec155318663d42ea2b94abd8849a
abelsonlive <brianabelson@gmail.com>
2014-06-06 19:12:43 -0400

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except (IOError, ImportError):
 # setup
 setup(
   name='siegfried',
-  version='0.0.1',
+  version='0.0.2',
   description="Tools for taming links.",
   long_description = description,
   classifiers=[
