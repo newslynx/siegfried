6143d825aca2394d120b3eff8a428f0e40fec6a3
abelsonlive <brianabelson@gmail.com>
2014-07-27 23:11:28 -0400

    vbump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except (IOError, ImportError):
 # setup
 setup(
   name='siegfried',
-  version='0.1.8',
+  version='0.1.9',
   description="Tools for taming lynx.",
   long_description = description,
   classifiers=[
